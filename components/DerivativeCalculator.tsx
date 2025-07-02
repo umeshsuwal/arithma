2f7d1fcfb1d43214c5562520581b33de85e255bd
Umesh Suwal <mail@umeshsuwal.com.np>
2025-07-02 08:07:22 +0545

    fixed some error

--- components/DerivativeCalculator.tsx
+++ components/DerivativeCalculator.tsx
@@ -8,7 +8,7 @@ import StepByStepSolution from "@/components/StepByStepSolution";
 
 export default function DerivativeCalculator() {
   const [expression, setExpression] = useState("");
-  const [variable, _setVariable] = useState("x");
+  const [variable] = useState("x");
   const [steps, setSteps] = useState<string[]>([]);
 
   const calculateDerivative = () => {
